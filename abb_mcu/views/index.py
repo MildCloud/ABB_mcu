2b3921ac1fefda6865c5913aee8d8e43e6e8d345
450Group6 <tian-shu@sjtu.edu.cn>
2024-07-17 13:51:29 +0800

    try GPIO + flask success

--- abb_mcu/views/index.py
+++ abb_mcu/views/index.py
@@ -4,6 +4,8 @@ from werkzeug.utils import secure_filename
 import os
 import ast
 import json
+import time
+import RPi.GPIO as GPIO
 
 import flask
 import abb_mcu
@@ -19,18 +21,32 @@ def _corsify_actual_response(response):
     return response
 
 
+
 @abb_mcu.app.route('/monitor', methods=['GET'])
 def handle_monitor():
     # TODO Return Real Monitor Data here
     monito_data = {'data1': 1, 'data2': 2}
     return _corsify_actual_response(flask.jsonify(monito_data))
 
+# SSR
+# Set up GPIO
+SSR_PIN = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SSR_PIN, GPIO.OUT)
 
 @abb_mcu.app.route('/toggle', methods=['POST'])
 def handle_toggle():
     req_dict_str = request.data.decode("UTF-8")
     req_dict = json.loads(req_dict_str)
     print('toggle require', req_dict['require'])
-    # TODO Add the code to toggle the real machine
+
+    if req_dict['require'] == 'on':
+        GPIO.output(SSR_PIN, GPIO.HIGH)
+    elif req_dict['require'] == 'off':
+        GPIO.output(SSR_PIN, GPIO.LOW)
     return _corsify_actual_response(flask.jsonify({}))
+
+
+
+
     
